133b86368e1797cb3d1de0a728371382464827ca
Zachary Allaun <zach.allaun@gmail.com>
2013-10-30 22:16:21 -0400

    Successfully querying Datomic REST

--- src/datomic_cljs/core.cljs
+++ src/datomic_cljs/core.cljs
@@ -3,14 +3,13 @@
             [datomic-cljs.http :as http])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
-(defn go-go-http! [url]
+(defn go-go-http! [options]
   (go
-    (let [res (<! (http/get url))]
+    (let [[_ res] (<! (http/get options))]
       (loop []
-        (let [piece (<! (:c-body res))]
-          (when-not (= piece :done)
-            (println piece)
-            (recur)))))))
+        (when-let [piece (<! (:c-body res))]
+          (println piece)
+          (recur))))))
 
 (defn go-go-go! []
   (go
@@ -19,6 +18,10 @@
       (.log js/console "go!"))))
 
 (defn -main [& args]
-  (go-go-http! "http://www.google.com"))
+  (go-go-http! {:protocol "http:"
+                :hostname "localhost"
+                :path "/api/query?q=%5B%3Afind+%3Fe+%3Fv+%3Ain+%24+%3Awhere+%5B%3Fe+%3Adb%2Fdoc+%3Fv%5D%5D&args=%5B%7B%3Adb%2Falias+%22db%2Fseattle%22%7D%5D&offset=&limit="
+                :port "9898"
+                :headers {"Accept" "application/edn"}}))
 
 (set! *main-cli-fn* -main)
--- src/datomic_cljs/http.cljs
+++ src/datomic_cljs/http.cljs
@@ -6,16 +6,30 @@
 (def js-http (nodejs/require "http"))
 
 (defn get
-  [url]
-  (let [c-res (async/chan)]
-    (.get js-http url
-          (fn [res]
-            (let [c-body (async/chan 10)]
-              (.setEncoding res "utf8")
-              (.on res "data" #(async/put! c-body %))
-              (.on res "end" #(async/put! c-body :done))
-              (async/put! c-res
-                          {:c-body c-body
-                           :status (.-statusCode res)
-                           :res res}))))
+  "Make an asyncronous GET request for the given options, returning a
+   core.async channel that will ultimately contain either [:success
+   response] or [:error error-object]. In the case of success, the
+   response will be a map containing:
+
+     :status, the HTTP status code;
+     :res, the Node.js response object;
+     :c-body, a core.async channel containing streamed response body
+              chunks (strings), which will be closed when streaming
+              is done."
+  [options]
+  (let [c-res (async/chan)
+        js-res (.get js-http (clj->js options)
+                     (fn [res]
+                       (let [c-body (async/chan 10)]
+                         (.setEncoding res "utf8")
+                         (.on res "data" #(async/put! c-body %))
+                         (.on res "end" #(async/close! c-body))
+                         (async/put! c-res
+                                     [:success {:c-body c-body
+                                                :status (.-statusCode res)
+                                                :res res}]
+                                     #(async/close! c-res)))))]
+    (.on js-res "error" #(async/put! c-res
+                                     [:error %]
+                                     (fn [] (async/close! c-res))))
     c-res))
