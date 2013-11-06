6bc1c7e39d0f17fb008b230922cf36ce0594542a
Zachary Allaun <zach.allaun@gmail.com>
2013-11-05 21:28:01 -0500

    Bump lein-cljsbuild version to 1.0.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  [org.clojure/clojurescript "0.0-1978"]
                  [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]]
 
-  :profiles {:dev {:plugins [[lein-cljsbuild "0.3.4"]]}}
+  :profiles {:dev {:plugins [[lein-cljsbuild "1.0.0-SNAPSHOT"]]}}
 
   :cljsbuild {:builds [{:id "dev"
                         :source-paths ["src"]
