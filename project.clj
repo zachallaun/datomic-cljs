20e7136f95fa188efd7e03d1c22b187022cd9930
Zachary Allaun <zach.allaun@gmail.com>
2013-11-15 09:56:03 -0500

    Bump to latest CLJS

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "MIT" :url "http://opensource.org/licenses/MIT"}
 
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1978"]
+                 [org.clojure/clojurescript "0.0-2030"]
                  [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]]
 
   :profiles {:dev {:plugins [[lein-cljsbuild "1.0.0-alpha2"]]}}
